52e51e455d8c54e368b02a6757437ad5833bef3e
Z11_falcon <st67053@student.upce.cz>
2024-11-07 08:15:38 +0100

    initial

---
+++ aplikace_zaznamnik-porad/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace aplikace_zaznamnik_porad
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
