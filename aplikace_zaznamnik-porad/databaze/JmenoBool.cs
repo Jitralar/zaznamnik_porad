dc4157942f59b77712287acdb3c3736ee02742ef
Z11_falcon <st67053@student.upce.cz>
2025-01-14 16:30:09 +0100

    konzultace WIP

---
+++ aplikace_zaznamnik-porad/databaze/JmenoBool.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aplikace_zaznamnik_porad.databaze
+{
+    internal class JmenoBool
+    {
+        public string Jmeno { get; set; }
+        public bool Check { get; set; }
+
+    }
+}
